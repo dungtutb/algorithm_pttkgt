797d84ec0c2be7ff21bcedcb937601d642606f33
dungtutb <phamdunghc1@gmail.com>
2017-09-26 21:41:54 +0700

    kosaraju

--- DFS.py
+++ DFS.py
@@ -1,11 +1,18 @@
 
-class Edge:
+class Vertex:
     
-    def __init__(self,start_time=0,end_time=0):
+    def __init__(self,index=0,start_time=0,end_time=0,status=0,group=0):
         self.start_time = start_time
         self.end_time = end_time
+        self.status = status
+        self.index = index
+        self.group = group
+    def reset(self):
+        self.start_time = 0
+        self.end_time = 0
         self.status = 0
-
+        self.group = 0
+        
 class MatranKe:
     def __init__(self,width,height):
         self.width = width
@@ -33,21 +40,28 @@ def Dfs(mang_vertex,matran,index,cur_time):
     cur_time += 1
     mang_vertex[index].status = 1
     for i in range(len(mang_vertex)):
-        if mang_vertex[i].status == 0 and matran.Arr[index][i]>0:
+        if mang_vertex[i].status == 0 and matran.Arr[mang_vertex[index].index][mang_vertex[i].index]>0:
+            mang_vertex[i].group = mang_vertex[index].group
             cur_time = Dfs(mang_vertex,matran,i,cur_time)
             cur_time += 1
     mang_vertex[index].status = 2
     mang_vertex[index].end_time = cur_time
     return cur_time  
 
+def reverse_matrix(matran):
+    for i in range(matran.height):
+        for j in range(i+1,matran.width):
+            temp = matran.Arr[i][j]
+            matran.Arr[i][j] = matran.Arr[j][i]
+            matran.Arr[j][i] = temp
 
 def program():
     
-    matran = init('c:\\Users\PHAM_DUNG\\myproject\\algorithm\\input.txt')
+    matran = init('c:\\Users\PHAM_DUNG\\myproject\\algorithm\\algorithm_pttkgt\\input.txt')
 
-    mang_vertex = [Edge() for i in range(matran.height)]
+    mang_vertex = [Vertex(index=i) for i in range(matran.height)]
 
-    Dfs(mang_vertex,matran,0,0)
+    Dfs(mang_vertex,matran,0,1)
 
     for i in range(len(mang_vertex)):
         print "dinh %d: %d %d" %(i+1,mang_vertex[i].start_time,mang_vertex[i].end_time)
--- Kosaraju.py
+++ Kosaraju.py
@@ -1,21 +1,35 @@
 import DFS
 
-def Kosaraju(mang_vertex,matran):
-    cur_time = 0
+def duyet(mang_vertex,matran):
+    cur_time = 1
     not_end=True
+    group = 1
+    # buoc 1
     while not_end:
         not_end=False
         for i in range(len(mang_vertex)):
             if mang_vertex[i].start_time == 0:
                 not_end = True
-                DFS.Dfs(mang_vertex,matran,i,cur_time)
+                mang_vertex[i].group = group
+                cur_time = DFS.Dfs(mang_vertex,matran,i,cur_time)
+                cur_time += 1
+                group += 1
                 break
+def Kosaraju(mang_vertex,matran):
+    duyet(mang_vertex,matran)
     for i in range(len(mang_vertex)):
-        print "dinh %d: %d %d" %(i+1,mang_vertex[i].start_time,mang_vertex[i].end_time)
-        
+        print "dinh %d: %d %d %d" %(i+1,mang_vertex[i].start_time,mang_vertex[i].end_time,mang_vertex[i].group)
+    #buoc 2  
+    DFS.reverse_matrix(matran)
+    #buoc 3
+    mang_vertex.sort(key= lambda ver: ver.end_time,reverse=True) # sap xep theo thu tu giam dan end_time
+    for i in range(len(mang_vertex)):
+        mang_vertex[i].reset()
+    duyet(mang_vertex,matran)
+    mang_vertex.sort(key=lambda ver: ver.index)
+    for i in range(len(mang_vertex)):
+        print "dinh %d: %d %d %d" %(i+1,mang_vertex[i].start_time,mang_vertex[i].end_time,mang_vertex[i].group)
 
-
-
-matran = DFS.init('c:\\Users\PHAM_DUNG\\myproject\\algorithm\\input_kosaraju.txt')
-mang_vertex = [DFS.Edge() for i in range(matran.height)]
+matran = DFS.init('c:\\Users\PHAM_DUNG\\myproject\\algorithm\\algorithm_pttkgt\\input_kosaraju.txt')
+mang_vertex = [DFS.Vertex(index=i) for i in range(matran.height)]
 Kosaraju(mang_vertex,matran)
